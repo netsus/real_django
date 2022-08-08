a808bb695bcee177fb6dca392d54220f0903c2b2
WonJun Cho <cwjcsk@naver.com>
2022-08-09 00:25:40 +0900

    1. black 적용 2. accounts, blog app 생성 3. accounts의 signup, login 구현 4. django-bootstrap-v5 설치 5. base.html 생성 6. from.html 생성 7. settings에서 password validator 모두 삭제

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = "accounts/form.html"
+    success_url = settings.LOGIN_URL
+
+
+signup = SignUpView.as_view()
+
+
+class MyLoginView(LoginView):
+    template_name = "accounts/form.html"
+
+
+login = MyLoginView.as_view()
+
+
+class MyLogoutView(LogoutView):
+    pass
